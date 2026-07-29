44bd20133a4ca67dd5ece7443fe5be0bce3e3eb7
SandiswaShange <sandiswashange32@gmail.com>
2026-07-29 10:01:13 +0200

    Removed WeatherForecast endpoint

--- GearShare.Api/obj/Debug/net10.0/GearShare.Api.AssemblyInfo.cs
+++ GearShare.Api/obj/Debug/net10.0/GearShare.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GearShare.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee482ca3e4824c48f2feb3e78dcd2dede82e014e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6f371326e365d5ae14a951ebd2fec148549936")]
 [assembly: System.Reflection.AssemblyProductAttribute("GearShare.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GearShare.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
